152a5cd7289910167f2bb188c939756c99f85b5b
Keila Islas <kislas@bsd220.org>
2024-02-22 10:00:09 -0600

    Added user location

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Keila Islas on 2/22/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
